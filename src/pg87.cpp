09f97d69ccb53c76c52bd805773f61f85c39a96c
ARKA1112 <bagha92@gmail.com>
2022-12-15 12:08:53 +0530

    .

---
+++ src/pg87.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string line;
+    auto p = 0;
+    //read input a line at a time until end of file
+    cout<<"enter a line";
+    while (getline(cin, line))
+    {
+        cout<<line;
+        if(line.empty())
+        {
+            break;
+        };
+        if (line.size()>=8)
+        {
+            p = line.size();
+            cout<<' '<<p<<endl;
+        };
+    };
+    return 0;
+}
