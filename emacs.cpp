a8a9fb43a462dd7cabe675b0998bc9158fb4a494
ARKA1112 <bagha92@gmail.com>
2023-01-18 20:11:23 +0530

    .

---
+++ emacs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  cout<<"Please enter your name ";
+  string s;
+  getline(cin,s);
+  cout<<"Hello "<<s<<endl;
+  cout<<"Have fun in emacs!"<<endl;
+  return 0;
+}
