f449b3a5cadc89008333e2da3cf9af93210b9e9a
ARKA1112 <bagha92@gmail.com>
2023-04-25 14:03:28 +0530

    new

--- main/pg228.cpp
+++ main/pg228.cpp
@@ -7,14 +7,20 @@ void print(const vector<string> ivec, vector<string>::const_iterator it)
 {
     if (it == ivec.end())
         return;
-    cout<<*it++<<endl;
-    print(ivec, it);
+    else
+        cout<<*it++<<endl;
+        print(ivec, it);
+        //cout<<"file name"<<__FILE__<<endl;
 }
 
+//Also includes ex 6.47
 int main()
 {
     const vector<string> ivec = {"deo","demon","slayer"};
     vector<string>::const_iterator it;
     it = ivec.begin();
+    cout<<"File name: "<<__FILE__
+        <<"Date: "<<__DATE__
+        <<"Time: "<<__TIME__<<endl;
     print(ivec,it);
 }
